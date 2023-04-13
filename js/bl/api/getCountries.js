6ee21a8eab479cc26297c4b0aa5d9fb1e6bfbebf
Fernando Solano <fersolano@ex2outcoding.local>
2023-04-13 00:32:10 -0600

    All america

--- js/bl/api/getCountries.js
+++ js/bl/api/getCountries.js
@@ -1,7 +1,5 @@
 export default async () => {
-  const result = await fetch(
-    'https://restcountries.com/v3.1/subregion/South%20America?fields=name,cca3,borders,latlng'
-  );
+  const result = await fetch('https://restcountries.com/v3.1/subregion/America?fields=name,cca3,borders,latlng');
   const data = await result.json();
   return data;
 };
